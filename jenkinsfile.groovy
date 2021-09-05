1e322dd5220673126753b8eb980039e7e899bf74
devops-srv <68237630+devops-srv@users.noreply.github.com>
2021-09-05 22:19:55 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any 
+    stages {
+        stage ("SCM checkout") {
+            steps {
+                git url:"https://github.com/devops-srv/webserverprojectA.git"
+            }
+        }
+        stage ("Archive Artifacts") {
+            steps {
+                archiveArtifacts '**/*.html'
+            }
+        }
+        stage ("transfer artifacts") {
+            steps {
+               sshPublisher(publishers: [sshPublisherDesc(configName: 'webserver', transfers: [sshTransfer(excludes: '', execCommand: '', execTimeout: 120000, flatten: true, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '**/*.html')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: true)]) 
+            }
+        }
+    }
+}
